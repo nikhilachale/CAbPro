51b493713c73cc4241696444204110e6eb16ccc0
nikhil achale <nicachale456@gmail.com>
2025-01-09 15:19:26 +0530

    for vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require("express");
 const { review } = require("./types");
 const { Review } = require("./db");
-const cors=require("cors")
+const cors=require("cors");
+const { get } = require('mongoose');
 const app = express();
 const port = process.env.PORT || 2000;
 app.use(cors({
@@ -13,6 +14,10 @@ app.use(cors({
 
 
 app.use(express.json());
+// for testing only 
+app,get((req, res) => {
+    res.send("Hello World");
+});
 
 app.post("/rev", async (req, res) => {
     const createpayload = req.body;
