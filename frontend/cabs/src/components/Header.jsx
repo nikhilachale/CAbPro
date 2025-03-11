def51c574417f7428f7f9efb8f2452b298544159
nikhil achale <nicachale456@gmail.com>
2025-03-11 23:14:57 +0530

    bug fix

--- frontend/cabs/src/components/Header.jsx
+++ frontend/cabs/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../images/cabbroo.png"
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="bg-blue-950 text-white flex items-center justify-between px-4 sm:px-8 py-4">
@@ -14,13 +14,14 @@ const Header = () => {
          <img src={logo} className="w-auto h-10"></img>
         </a>
       </h1>
+<Link to="/contact">
 
-      <a
-        href="tel:+919111034494"
+      <button
         className="bg-white hover:bg-blue-700 text-blue-950 font-bold py-2 px-4 rounded text-sm sm:text-base"
       >
-        Call Now
-      </a>
+      Call Now
+      </button>
+      </Link>
     </div>
   );
 };
